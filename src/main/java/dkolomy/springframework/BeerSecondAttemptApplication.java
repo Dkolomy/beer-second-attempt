f255df30bda7dcd4d766dd357ab7e607d1f954d2
Dkolomy <Dkolomy@hotmail.com>
2022-05-11 23:26:28 -0400

    Add files via upload

---
+++ src/main/java/dkolomy/springframework/BeerSecondAttemptApplication.java
@@ -0,0 +1,13 @@
+package dkolomy.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerSecondAttemptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerSecondAttemptApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dkolomy/springframework/web/model/BeerDto.java
@@ -0,0 +1,5 @@
+package dkolomy.springframework.web.model;
+
+public class BeerDto {
+
+}
